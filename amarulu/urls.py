cbb57b9b65b041f506020ebd869363f37a5fddb8
Rathnarajugudala <gudalarathnaraju821@gmail.com>
2025-07-23 08:30:54 +0530

    owief

--- amarulu/urls.py
+++ amarulu/urls.py
@@ -1,9 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
-    path("__reload__/", include("django_browser_reload.urls"))
+    path('',include('app.urls'))
 ]
+
+if settings.DEBUG:
+    import django_browser_reload
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+
